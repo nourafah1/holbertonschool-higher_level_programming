59a7bc70dd41b808ba665a5305de63cc7f42cf06
noura <noura.fahad2022@gmail.com>
2026-06-06 05:54:15 +0000

    Fix sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-word = "Holberton"
-print(word[6:] + word[:6])
+str = "Python is an interpreted, interactive, object-oriented programming \
+language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
