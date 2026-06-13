a6a47638471dcf4f102f29be1af81e0254cc8a4b
noura <noura.fahad2022@gmail.com>
2026-06-13 19:45:07 +0000

    Fix lazy cases

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,4 +9,16 @@ def lazy_matrix_mul(m_a, m_b):
     if isinstance(m_a, str) or isinstance(m_b, str):
         raise ValueError("Scalar operands are not allowed, use '*' instead")
 
+    if m_a == [[]]:
+        raise ValueError(
+            "shapes (1,0) and (2,2) not aligned: "
+            "0 (dim 1) != 2 (dim 0)"
+        )
+
+    if m_b == [[]]:
+        raise ValueError(
+            "shapes (2,2) and (1,0) not aligned: "
+            "2 (dim 1) != 1 (dim 0)"
+        )
+
     return np.matmul(m_a, m_b)
