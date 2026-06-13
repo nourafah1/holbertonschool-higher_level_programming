f0dcea97e53b221d1ef27944114ea573e75f2a1f
noura <noura.fahad2022@gmail.com>
2026-06-13 19:42:10 +0000

    Fix lazy mul

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -6,4 +6,7 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """Multiply 2 matrices using NumPy"""
+    if isinstance(m_a, str) or isinstance(m_b, str):
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
+
     return np.matmul(m_a, m_b)
