565ed21e88026b63b5ba90201f9eb413a21d49bc
noura <noura.fahad2022@gmail.com>
2026-06-13 17:32:05 +0000

    Fix add integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,10 +5,10 @@
 def add_integer(a, b=98):
     """Adds two integers and returns the result"""
 
-    if not isinstance(a, (int, float)):
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
 
-    if not isinstance(b, (int, float)):
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
