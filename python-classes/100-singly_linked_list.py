f20f1b56f9de76ca1332b64998e7b60fb71224bd
noura <noura.fahad2022@gmail.com>
2026-06-20 16:55:30 +0300

    Add singly linked list

---
+++ python-classes/100-singly_linked_list.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Module that defines a singly linked list."""
+
+
+class Node:
+    """Defines a node of a singly linked list."""
+
+    def __init__(self, data, next_node=None):
+        """Initialize node."""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Retrieve data."""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Set data."""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Retrieve next node."""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Set next node."""
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Defines a singly linked list."""
+
+    def __init__(self):
+        """Initialize empty list."""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Insert node in sorted order."""
+        new_node = Node(value)
+
+        if self.__head is None or value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+            return
+
+        current = self.__head
+
+        while (current.next_node is not None and
+               current.next_node.data < value):
+            current = current.next_node
+
+        new_node.next_node = current.next_node
+        current.next_node = new_node
+
+    def __str__(self):
+        """Return string representation of list."""
+        values = []
+        current = self.__head
+
+        while current is not None:
+            values.append(str(current.data))
+            current = current.next_node
+
+        return "\n".join(values)
