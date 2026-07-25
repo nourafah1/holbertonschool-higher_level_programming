3af03386bdc94c151a077d8f34f7add6488e9ce6
noura <noura.fahad2022@gmail.com>
2026-07-25 23:40:43 +0300

    Fix city listing

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -3,7 +3,7 @@
 
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import joinedload, sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
@@ -21,12 +21,17 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
+    states = (
+        session.query(State)
+        .options(joinedload(State.cities))
+        .order_by(State.id)
+        .all()
+    )
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
 
-        for city in sorted(state.cities, key=lambda city: city.id):
+        for city in sorted(state.cities, key=lambda item: item.id):
             print("\t{}: {}".format(city.id, city.name))
 
     session.close()
