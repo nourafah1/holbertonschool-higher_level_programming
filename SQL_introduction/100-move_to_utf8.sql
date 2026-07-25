ae27ad75cc5b48011543e8be725563da9920da74
noura <noura.fahad2022@gmail.com>
2026-07-25 19:05:06 +0300

    Fix database selection

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -3,7 +3,7 @@ ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Convert the table to utf8mb4
-ALTER TABLE first_table
+-- Convert the table and its text fields to utf8mb4
+ALTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
