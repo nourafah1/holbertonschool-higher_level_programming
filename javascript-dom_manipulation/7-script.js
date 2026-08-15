dfa3aa77ff87f06332eb2a27581486f4d811a58f
noura <noura.fahad2022@gmail.com>
2026-08-15 21:11:41 +0300

    Fetch and list Star Wars movies

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,11 @@
+const listMovies = document.querySelector('#list_movies');
+
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    data.results.forEach(movie => {
+      const listItem = document.createElement('li');
+      listItem.textContent = movie.title;
+      listMovies.appendChild(listItem);
+    });
+  });
